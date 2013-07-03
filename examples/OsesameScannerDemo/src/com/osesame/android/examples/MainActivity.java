e63a521c35dac3dbc2926be291c783df4661b68f
Sutee Sudprasert <etipitaka@gmail.com>
2013-07-03 12:38:23 +0700

    rename folder

---
+++ examples/OsesameScannerDemo/src/com/osesame/android/examples/MainActivity.java
@@ -0,0 +1,59 @@
+package com.osesame.android.examples;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+import com.osesame.android.scanner.OsesameConstants;
+import com.osesame.android.scanner.OsesamePhotoScannerActivity;
+import com.osesame.android.scanner.OsesameScannerActivity;
+
+public class MainActivity extends Activity {
+
+  private static final int SCANNER_REQUEST = 0;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+
+  public void launchScanner(View v) {
+    if (isCameraAvailable()) {
+      Intent intent = new Intent(this, OsesameScannerActivity.class);
+      startActivityForResult(intent, SCANNER_REQUEST);
+    } else {
+      Toast.makeText(this, "Rear Facing Camera Unavailable", Toast.LENGTH_SHORT).show();
+    }
+  }
+
+  public void launchPhotoScanner(View v) {
+    Intent intent = new Intent(this, OsesamePhotoScannerActivity.class);
+    startActivityForResult(intent, SCANNER_REQUEST);
+  }
+
+  public boolean isCameraAvailable() {
+    PackageManager pm = getPackageManager();
+    return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    switch (requestCode) {
+      case SCANNER_REQUEST:
+        if (resultCode == RESULT_OK) {
+          Toast.makeText(this, "Scan Result = " + data.getStringExtra(OsesameConstants.SCAN_RESULT),
+              Toast.LENGTH_SHORT).show();
+        } else if(resultCode == RESULT_CANCELED && data != null) {
+          String error = data.getStringExtra(OsesameConstants.ERROR_INFO);
+          if(!TextUtils.isEmpty(error)) {
+            Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+          }
+        }
+        break;
+    }
+  }
+}
